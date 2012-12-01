27527cd7b285c196b4281a174db8258c9eeedbff
Doron <doron2402@gmail.com>
2012-11-30 16:58:09 -0800

    First Upload - Button Counter for website event

---
+++ Config.php
@@ -0,0 +1,10 @@
+﻿<?php
+/*
+	ConfigFile.php
+*/
+
+define("XML_FILENAME","Buttons.xml"); //The xml file where we going to show our counts
+define("XML_PATH",""); //the xml path if any leave blank else dont forget to add "/" at the end
+
+
+?>
---
+++ CounterAjax.php
@@ -0,0 +1,48 @@
+﻿<?php
+if(isset($_GET['ButtonId']) && $_GET['ButtonId'] > 0)
+{
+
+	require('Config.php');
+	$Id = (int)$_GET['ButtonId'];
+	if (file_exists(XML_PATH.XML_FILENAME)) 
+	{
+		$xml = file_get_contents(XML_PATH.XML_FILENAME);
+		$Events = new SimpleXMLElement($xml);
+		$New_XML = '<?xml version="1.0" encoding="UTF-8"?>';
+		$New_XML .= '<Events>';
+		foreach ($Events->Event as $Event)
+		{
+			if($Event->Id == $Id)
+			{
+				$newCounter = $Event->Counter + 1;
+				$New_XML .=	'<Event><Id>'.$Event->Id .'</Id><Counter>'.$newCounter.'</Counter></Event>';
+			}
+			else
+			{
+				$New_XML .=	'<Event><Id>'.$Event->Id .'</Id><Counter>'.$Event->Counter.'</Counter></Event>';
+			}
+		}
+		$New_XML .= '</Events>';
+		
+		//Save the new xml
+		$myFile = XML_PATH.XML_FILENAME;
+		$fh = fopen($myFile, 'w') or die("can't open file");
+		fwrite($fh, $New_XML);
+		$Process = fclose($fh);
+		if($Process)
+		{
+			echo 'Success';
+		}
+	}
+	else 
+	{
+		die("Can't find the xml file");
+	}
+	
+}
+else
+{
+	die("Wrong Id");
+}
+
+?>
