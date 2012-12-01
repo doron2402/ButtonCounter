8de0713309adc6857bc7b7ef68155f7e1ef58151
Doron <doron2402@gmail.com>
2012-11-30 17:33:25 -0800

    1

--- index.php
+++ index.php
@@ -51,6 +51,41 @@ function Counter(Id)
 			<h1>Event Counter</h1>
 		</div>
 		<div id="mainbody">
+			<h2>Here are the steps:</h2>
+			<ul>
+			<li>
+			<p>On every event you want to follow add this function <xmp>onClick="Counter(1);"</xmp>we use number one as example you should change the id according to you.</p>
+			</li>
+			<li>
+			Dont forget to add the the followin javascript file<br />
+			First - add jquery to your code.
+			Second the function:
+			<xmp>
+<script type="text/javascript">
+function Counter(Id)
+{
+	var Id = parseInt(Id);
+	var Url = 'CounterAjax.php';
+	$.ajax({
+	 type: "POST",
+	  url: Url,
+	  data: 'ButtonId=' + Id,
+	  success: function(data) {
+		console.log(data);
+	  }
+	});
+}
+</script>
+			</xmp>
+			</li>
+			<li>
+			That's it you good to go...
+			You can check those 3 buttons and see on the console log that the script is working great.
+			</li>
+			</ul>
+			
+			</pre>
+			
 			<button onClick="Counter(1);">Button 1</button>
 			<button onClick="Counter(2);">Button 2</button>
 			<button onClick="Counter(3);">Button 3</button>
